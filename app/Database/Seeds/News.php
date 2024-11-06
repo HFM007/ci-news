06ba4ce468f658b64e9ccfd5f2ac7c60ec43acbc
HFM007 <habib.fm20@student.unibi.ac.id>
2024-11-06 09:52:47 +0700

    Add seed data for news articles in News seeder

--- app/Database/Seeds/News.php
+++ app/Database/Seeds/News.php
@@ -8,6 +8,28 @@ class News extends Seeder
 {
     public function run()
     {
-        //
+        //membuat data
+        $news_data = [
+            [
+                'title' => 'Selamat Datang di CodeIgniter 4',
+                'slug'  => 'CodeIgniter Intro',
+                'content' => 'Pengenalan CodeIgniter 4 untuk pemula'
+            ],
+            [
+                'title' => 'Hello World',
+                'slug'  => 'Hello World',
+                'content' => 'Hello World dengan CodeIgniter 4'
+            ],
+            [
+                'title' => 'Meetup komunitas CodeIgniter',
+                'slug'  => 'Meetup CodeIgniter',
+                'content' => 'Meetup CodeIgniter di Jakarta'
+            ]
+        ];
+
+        foreach ($news_data as $data) {
+            // insert data ke database
+            $this->db->table('news')->insert($data);
+        }
     }
 }
